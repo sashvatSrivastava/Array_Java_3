56ce0e7730cfacb435f149e31d5d72e895f8277e
Sashvat Srivastava <67827299+sashvatSrivastava@users.noreply.github.com>
2021-01-29 17:34:42 +0530

    Add files via upload

---
+++ Array/src/ArrayNew.java
@@ -0,0 +1,10 @@
+public class ArrayNew {
+    public static void main(String args[]) {
+        String color[] = {"red","green","blue","orange","brown","black","grey","purple"};
+        int i;
+        for(i = 0; i < color.length ; i++)
+        {
+            System.out.println("My favorite color is " + color[i] + "\n");
+        }
+    }
+}
